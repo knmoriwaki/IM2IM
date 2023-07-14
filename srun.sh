4805321f7c6e826d028e8c48365907443376fe62
scarletto999 <s.stadtler@fz-juelich.de>
2023-07-14 15:06:29 +0900

    Modifications to srun to run addionally the evaluation for each xai experiment. Under development.

--- srun.sh
+++ srun.sh
@@ -2,27 +2,59 @@
 
 #export CUDA_LAUNCH_BLOCKING=1 # for debugging
 
-val_dir=/mnt/data_cat4/moriwaki/IM2IM/val_data
-test_dir=/mnt/data_cat4/moriwaki/IM2IM/val_data
-results_dir=./output
+expected_hostname="cat4"
+current_hostname=$(hostname)
 
-model=pix2pix_2
-name=pix2pix_2_bs4_ep1_lambda1000_vanilla
-output_dir=/mnt/data_cat4/moriwaki/IM2IM/output/${name}
+if [ "$current_hostname" != "$expected_hostname" ]; then
+    echo "Current host: $current_hostname"
+    echo "Please login to $expected_hostname to execute this script."
+    exit
+else
+    echo "Running IM2IM GAN pix2pix inference and XAI experiments on $current_hostname"
+    test_python_env=infernce_venv
+    xai_python_env=xai_venv
 
-norm=2.0e-7
-load_iter=-1
-igpu=1
+    val_dir=/mnt/data_cat4/moriwaki/IM2IM/val_data
+    test_dir=/mnt/data_cat4/moriwaki/IM2IM/val_data
+    results_dir=./output
 
-# Test if all experiments still run
-# Normal testing
-#python main.py --name $name --gpu_ids $igpu --test_dir $test_dir --output_dir $output_dir --results_dir $results_dir --nrun 100 --model $model --load_iter $load_iter --norm $norm 
+    model=pix2pix_2
+    name=pix2pix_2_bs4_ep1_lambda1000_vanilla
+    output_dir=/mnt/data_cat4/moriwaki/IM2IM/output/${name}
 
-# XAI testing
-experiments=("ha" "oiii" "random" "random_ha" "random_oiii" "faint_ha" "occlusion")
-for xai_exp in "${experiments[@]}"
-do
-    python main.py --name $name --gpu_ids $igpu --test_dir $test_dir --output_dir $output_dir --results_dir $results_dir --nrun 100 --model $model --load_iter $load_iter --norm $norm --isXAI --xai_exp $xai_exp
-done
+    norm=2.0e-7
+    load_iter=-1
+    igpu=1
+
+    . ${test_python_env}/bin/activate
+    which python
+    # Test if all experiments still run
+    # Normal testing
+    #python main.py --name $name --gpu_ids $igpu --test_dir $test_dir --output_dir $output_dir --results_dir $results_dir --nrun 100 --model $model --load_iter $load_iter --norm $norm 
+
+    # XAI testing
+    experiments=("ha" "oiii" "random" "random_ha" "random_oiii" "faint_ha" "occlusion")
+    for xai_exp in "${experiments[@]}"
+    do
+        #python main.py --name $name --gpu_ids $igpu --test_dir $test_dir --output_dir $output_dir --results_dir $results_dir --nrun 100 --model $model --load_iter $load_iter --norm $norm --isXAI --xai_exp $xai_exp
+        echo " Mocking the inference $xai_exp"
+    done
+    deactivate
+
+    . $xai_python_env/bin/activate
+    which python
+    # Evaluation of the inference on the test set
+    python eval.py --output_dir ${results_dir} --results_dir ${results_dir} --nrun 100 --isRef
+
+    # XAI experiment evaluation
+    for xai_exp in "${experiments[@]}"
+    do
+        python eval.py --output_dir ${results_dir}  --results_dir ${results_dir} --nrun 100 --xai_exp xai_exp_${xai_exp}
+    done
+    deactivate
+fi
+
+
+
 
 
